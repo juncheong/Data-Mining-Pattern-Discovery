5a53095748f456d461331f1a2123540d89ebe37d
Jun <jcheong@tailoredindustry.com>
2019-05-19 23:04:56 -0400

    uploaded all data sets

--- src/Database.java
+++ src/Database.java
@@ -1,3 +1,7 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Database {
@@ -7,6 +11,7 @@ public class Database {
     private String DB_PASSWORD;
     private String DB_PORT;
     private String DB_NAME;
+    private String JDBC_URL;
     
     public Database(String dB_HOST, String dB_USERNAME, String dB_PASSWORD, String dB_PORT, String dB_NAME) {
         DB_HOST = dB_HOST;
@@ -14,33 +19,152 @@ public class Database {
         DB_PASSWORD = dB_PASSWORD;
         DB_PORT = dB_PORT;
         DB_NAME = dB_NAME;
+        JDBC_URL = "jdbc:mysql://" + dB_HOST + ":" + dB_PORT + "/" + dB_NAME;
     }
 
     public void uploadDataSets(Scanner adherence, Scanner allLog, Scanner behavior, Scanner selectedLog, Scanner selfEfficacy){
-        uploadAdherence(adherence);
-        uploadAllLog(allLog);
-        uploadBehavior(behavior);
-        uploadSelectedLog(selectedLog);
-        uploadSelfEfficacy(selfEfficacy);
+        try {
+            
+            Connection connection = DriverManager.getConnection(JDBC_URL, DB_USERNAME, DB_PASSWORD);
+            Statement statement = connection.createStatement();
+            
+            uploadAdherence(adherence, statement);
+            uploadAllLog(allLog, statement);
+            uploadBehavior(behavior, statement);
+            uploadSelectedLog(selectedLog, statement);
+            uploadSelfEfficacy(selfEfficacy, statement);
+            
+        } catch (SQLException e){
+            System.out.println(e);
+        }
     }
     
-    private void uploadAdherence(Scanner adherence){
+    private void uploadAdherence(Scanner adherence, Statement statement){
+        String tableName = "adherence_response";
+        
+        while (adherence.hasNextLine()){
+            try{
+                String[] line = adherence.nextLine().split("\\s+");
+                StringBuilder query = new StringBuilder("INSERT INTO " + tableName + " VALUES (");
+                
+                for(int i = 0; i < line.length; i++){
+                    query.append("'" + line[i] + "'");
+                    if (i != line.length-1)
+                        query.append(", ");
+                }
+                
+                query.append(");");
+                statement.executeUpdate(query.toString());
+            } catch (SQLException e){
+                System.out.println(e);
+            }
+        }
         
     }
     
-    private void uploadAllLog(Scanner allLog){
+    private void uploadAllLog(Scanner allLog, Statement statement){
+        String tableName = "all_log";
         
+        while (allLog.hasNextLine()){
+            try{
+                String[] line = allLog.nextLine().split("\\s+");
+                StringBuilder query = new StringBuilder("INSERT INTO " + tableName + " VALUES ("); 
+                query.append("'" + line[0] + "', ");
+                query.append("'" + line[1] + " " + line[2] + "'");
+                
+                if (line.length > 3){
+                    query.append(", '");
+                    StringBuilder description = new StringBuilder();
+                    
+                    for (int i = 3; i < line.length; i++){
+                        description.append(line[i]);
+                        if (i != line.length-1)
+                            description.append(" ");
+                    }
+                    query.append(description + "'");
+                    
+                }
+                
+                query.append(");");
+                statement.executeUpdate(query.toString());
+            } catch (SQLException e){
+                System.out.println(e);
+            }
+        }
     }
     
-    private void uploadBehavior(Scanner behavior){
+    private void uploadBehavior(Scanner behavior, Statement statement){
+        String tableName = "behavior_score";
         
+        while (behavior.hasNextLine()){
+            try{
+                String[] line = behavior.nextLine().split("\\s+");
+                StringBuilder query = new StringBuilder("INSERT INTO " + tableName + " VALUES ("); 
+                query.append("'" + line[0] + "', ");
+                query.append("'" + line[1] + "', ");
+                query.append("'" + line[2] + "', ");
+                query.append("'" + line[3] + "', ");
+                query.append("'" + line[4] + "'");
+                
+                query.append(");");
+                statement.executeUpdate(query.toString());
+            } catch (SQLException e){
+                System.out.println(e);
+            }
+        }
     }
     
-    private void uploadSelectedLog(Scanner selectedLog){
+    private void uploadSelectedLog(Scanner selectedLog, Statement statement){
+        String tableName = "selected_log";
         
+        while (selectedLog.hasNextLine()){
+            try{
+                String[] line = selectedLog.nextLine().split("\\s+");
+                StringBuilder query = new StringBuilder("INSERT INTO " + tableName + " VALUES ("); 
+                query.append("'" + line[0] + "', ");
+                query.append("'" + line[1] + "', ");
+                query.append("'" + line[2] + " " + line[3] + "'");
+                
+                if (line.length > 4){
+                    query.append(", '");
+                    StringBuilder description = new StringBuilder();
+                    
+                    for (int i = 4; i < line.length; i++){
+                        description.append(line[i]);
+                        if (i != line.length-1)
+                            description.append(" ");
+                    }
+                    query.append(description + "'");
+                    
+                }
+                
+                query.append(");");
+                statement.executeUpdate(query.toString());
+            } catch (SQLException e){
+                System.out.println(e);
+            }
+        }
     }
     
-    private void uploadSelfEfficacy(Scanner selfEfficacy){
+    private void uploadSelfEfficacy(Scanner selfEfficacy, Statement statement){
+        String tableName = "self_efficacy";
         
+        while (selfEfficacy.hasNextLine()){
+            try{
+                String[] line = selfEfficacy.nextLine().split("\\s+");
+                StringBuilder query = new StringBuilder("INSERT INTO " + tableName + " VALUES ("); 
+                for(int i = 0; i < line.length; i++){
+                    query.append("'" + line[i] + "'");
+                    if (i != line.length-1)
+                        query.append(", ");
+                }
+                
+                query.append(");");
+                statement.executeUpdate(query.toString());
+                System.out.println(query.toString());
+            } catch (SQLException e){
+                System.out.println(e);
+            }
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -8,12 +9,12 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            Scanner environmentVariables = new Scanner(new File("env"));
-            Scanner adherence = new Scanner(new File("/data/adherence.txt"));
-            Scanner allLog = new Scanner(new File("/data/all-log.txt"));
-            Scanner behavior = new Scanner(new File("/data/behavior.txt"));
-            Scanner selectedLog = new Scanner(new File("/data/selected-log.txt"));
-            Scanner selfEfficacy = new Scanner(new File("/data/self-efficacy.txt"));
+            Scanner environmentVariables = new Scanner(Paths.get("env"));
+            Scanner adherence = new Scanner(Paths.get("data/adherence.txt"));
+            Scanner allLog = new Scanner(Paths.get("data/all-log.txt"));
+            Scanner behavior = new Scanner(Paths.get("data/behavior.txt"));
+            Scanner selectedLog = new Scanner(Paths.get("data/selected-log.txt"));
+            Scanner selfEfficacy = new Scanner(Paths.get("data/self-efficacy.txt"));
             
             env = loadEnvVar(environmentVariables);
             environmentVariables.close();
@@ -22,20 +23,27 @@ public class Main {
                                             env.get("DB_PASSWORD"), env.get("DB_PORT"), 
                                             env.get("DB_NAME"));
             
-            database.uploadDataSets(adherence, allLog, behavior, selectedLog, selfEfficacy);
+            //database.uploadDataSets(adherence, allLog, behavior, selectedLog, selfEfficacy);
             adherence.close();
             allLog.close();
             behavior.close();
             selectedLog.close();
             selfEfficacy.close();
             
-        } catch(FileNotFoundException e){
+        } catch(IOException e){
             System.out.println(e);
         }
     }
     
     private static Map<String, String> loadEnvVar(Scanner environmentVariables){
+        Map<String, String> hashMap = new HashMap<String, String>();
         
+        while(environmentVariables.hasNextLine()){
+            String[] variable = environmentVariables.nextLine().split("=");
+            hashMap.put(variable[0], variable[1]);
+        }
+        
+        return hashMap;
     }
 
 }
